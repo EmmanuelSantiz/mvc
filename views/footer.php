37d828d64b984665d97673e30f9e851b00c74ee4
EmmanuelSantiz <emmanuel.07.01@hotmail.com>
2018-10-05 17:29:54 -0500

    ultimo del dia

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 </main>
 <footer class="footer">
     <div class="container">
-        <span class="text-muted">@ Emmanuel Santis <?php echo date('d/m/Y'); ?></span>
+        <span class="text-muted">@ Emmanuel Santiz <?php echo date('Y'); ?></span>
     </div>
 </footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
